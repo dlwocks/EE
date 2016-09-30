2a83640a27f94469c9bfd7e7e9cca4d17882dd4d
dlwocks <frank_smic@hotmail.com>
2016-09-30 19:05:36 +0800

    using slope and bfgs - still have bugs

--- tttlearning.py
+++ tttlearning.py
@@ -1,5 +1,5 @@
 from numpy import array, dot, log, e
-from scipy.optimize import fmin
+from scipy.optimize import minimize
 from random import random
 
 from ttttester import exhaustive_check
@@ -12,11 +12,22 @@ def _sigmoid(z):
 def costfunc(theta, data, ans):
     return sum(ans * -log(_sigmoid(dot(data, theta))) - (1 - ans) * log(1 - _sigmoid(dot(data, theta))))
 
+def costfunc_d(theta, data, ans):
+    return dot(data.T, (_sigmoid(dot(data, theta)) - ans)) / len(theta)
+
+
+testdata = array([[0, 0, 0, 0, 1, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 1, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 1, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 1, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 1, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 1, 0, 0, 0, 0]])
+testans = array([1, 1, 1, 1, 1, 0])
 
 if __name__ == '__main__':
     dk = exhaustive_check()
     data, ans = dk.fetch_data_array()
     theta = array([0 for i in range(9)])
     print(theta)
-    theta = fmin(costfunc, theta, args=(data, ans),maxfun=100000)
+    theta = minimize(costfunc, theta, args=(data, ans), jac=costfunc_d, method='BFGS')
     print(theta)
