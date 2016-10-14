506f92e53a54b08ff09c5d261495b41c1171e64a
dlwocks <frank_smic@hotmail.com>
2016-10-14 10:01:23 +0800

    ctsur added

--- logreg_ai.py
+++ logreg_ai.py
@@ -36,6 +36,27 @@ def _orboard(board):
     m = max(board)
     return [0 if not i else (m - i + 1) if i % 2 else -(m - i + 1) for i in board]
 
+ctsur_map = {0: [1, 3, 4],
+             1: [0, 2, 3, 4, 5],
+             2: [1, 4, 5],
+             3: [0, 1, 4, 6, 7],
+             4: [0, 1, 2, 3, 5, 6, 7, 8],
+             5: [1, 2, 4, 7, 8],
+             6: [3, 4, 7],
+             7: [3, 4, 5, 6, 8],
+             8: [4, 5, 7]}
+
+
+def _ctsur(board):  # with board: 1.06
+    ret = [0 for i in range(18)]
+    for i in range(9):
+        for p in ctsur_map[i]:
+            if board[p] != 0 and board[p] % 2 == 0:
+                ret[i] += 1
+            elif board[p] % 2 == 1:
+                ret[i+9] += 1
+    return ret
+
 
 class logreg_ai(object):
     feature_num = 9
@@ -44,13 +65,15 @@ class logreg_ai(object):
                         'nboard': _nboard,
                         'lboard': _lboard,
                         'oboard': _oboard,
-                        'orboard': _orboard}
+                        'orboard': _orboard,
+                        'ctsur': _ctsur}
     FEATURE_NUM_MAP = {'board': 9,
                        'abs': 9,
                        'nboard': 9,
                        'lboard': 9,
                        'oboard': 9,
-                       'orboard': 9}
+                       'orboard': 9,
+                       'ctsur': 18}
 
     def __init__(self, t=None, feature=['board']):
         self.data = []
