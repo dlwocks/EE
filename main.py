03d75cfa1df107b43dc555147931475ca9187d29
dlwocks <frank_smic@hotmail.com>
2016-10-20 14:19:10 +0800

    main.py script modify

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import logreg_ai
-import ai
 import ttttester
 from statistics import mean, stdev, variance
 
@@ -15,16 +14,16 @@ if __name__ == '__main__':
     # complete_check(algorithm=ai.getstep, pt=True)
     # complete_check(algorithm=randomstep, pt=True)
     TRY = 10
-    timerec = []
+    scorerec = []
     try:
         for i in range(TRY):
             if i % min(10, TRY//10) == 0 and i:
                 print('%d ais checked..' % i)
             ai = logreg_ai.logreg_ai(feature=['board'])
-            ai.startlearn(game=1000, opponent='random', pt=False, graph=False)
-            time = ttttester.complete_check(ai.getstep, pt=False)
-            # time = complete_check(randomstep, pt=False)
-            timerec.append(time)
-        print('Finished check.\nAverage AEP for this ai is: %f\nStandard deviation for AEP is:%f' % (mean(timerec), stdev(timerec)))
+            # ai = ann_ai.ann_ai(layernum=[9, 18, 9])
+            ai.startlearn(game=100, opponent='random', pt=False, graph=False)
+            score = ttttester.complete_check(ai.getstep, pt=False)
+            scorerec.append(score)
+        print('Finished check.\nAverage AEP for this ai is: %f\nStandard deviation for AEP is:%f' % (mean(scorerec), stdev(scorerec)))
     except KeyboardInterrupt:
-        print('Check interrupted with %d try.\nAverage AEP for this ai is: %f\nStandard deviation for AEP is:%f' % (i, mean(timerec), stdev(timerec)))
+        print('Check interrupted with %d try.\nAverage AEP for this ai is: %f\nStandard deviation for AEP is:%f' % (i, mean(scorerec), stdev(scorerec)))
