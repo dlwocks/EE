d5aa134ad9f94f6bcabe8d84d66f333877225d8e
dlwocks <frank_smic@hotmail.com>
2016-10-20 14:10:09 +0800

    ann_ai: inherit from base, getstep from fowardprop

--- ann_ai.py
+++ ann_ai.py
@@ -1,11 +1,13 @@
 from learningfunc import ann
+from base_ai import base_ai
 
 
-class ann_ai(object):
+class ann_ai(base_ai):
     def __init__(self, layernum):
         self.ann = ann(layernum)
 
     def getstep(self, board, ainum, step):
+        returned = self.ann.fowardprop(board)
         raise NotImplementedError
 
     def startlearn(self, game='converge', difftol=0.01, opponent='random', graph=True, pt=True):
