480621cbc891cce83566b58830e27e39031c16de
dlwocks <frank_smic@hotmail.com>
2016-10-20 11:52:43 +0800

    main.py script changes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from logreg_ai import logreg_ai
-from ai import algorithm_wiki
-from ttttester import complete_check, randomstep
+import logreg_ai
+import ai
+import ttttester
 from statistics import mean, stdev, variance
 
 '''
@@ -11,18 +11,20 @@ board       1.17    0.10
 abs         0.93    0.08
 '''
 if __name__ == '__main__':
-    #ai = logreg_ai().startlearn(game=100, opponent='random', pt=False, graph=False)
-    #complete_check(algorithm=ai.getstep, pt=True)
+    # ai = logreg_ai().startlearn(game=100, opponent='random', pt=False, graph=False)
+    # complete_check(algorithm=ai.getstep, pt=True)
     # complete_check(algorithm=randomstep, pt=True)
-    TRY = 100
-    timerec =[]
+    TRY = 10
+    timerec = []
     try:
         for i in range(TRY):
-            if i % 5 == 0 and i:
+            if i % min(10, TRY//10) == 0 and i:
                 print('%d ais checked..' % i)
-            time = complete_check(logreg_ai(feature=['board']).startlearn(game=100, opponent='random', pt=False, graph=False).getstep, pt=False)
+            ai = logreg_ai.logreg_ai(feature=['board'])
+            ai.startlearn(game=1000, opponent='random', pt=False, graph=False)
+            time = ttttester.complete_check(ai.getstep, pt=False)
             # time = complete_check(randomstep, pt=False)
             timerec.append(time)
-        print('Finished check.\nAverage AEP for this ai is: %f\nStandard deviation for AEP is:%f\nVariance:%f' % (mean(timerec), stdev(timerec), variance(timerec)))
+        print('Finished check.\nAverage AEP for this ai is: %f\nStandard deviation for AEP is:%f' % (mean(timerec), stdev(timerec)))
     except KeyboardInterrupt:
-        print('Check interrupted with %d try.\nAverage AEP for this ai is: %f\nStandard deviation for AEP is:%f\nVariance:%f' % (i, mean(timerec), stdev(timerec), variance(timerec)))
+        print('Check interrupted with %d try.\nAverage AEP for this ai is: %f\nStandard deviation for AEP is:%f' % (i, mean(timerec), stdev(timerec)))
