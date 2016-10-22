94ed59570ce6f4b4d25b67770959910b596e1ca3
dlwocks <frank_smic@hotmail.com>
2016-10-22 12:02:43 +0800

    ann bug fixing: successful convergent on subdata/ans

--- learningfunc.py
+++ learningfunc.py
@@ -79,17 +79,19 @@ class ann(object):
         self.totalthetalen = self.partialthetalen[-1]
         self.layernum = layernum
         self.layercount = len(layernum)
-        if theta:
+        if theta is not None:
             if not isinstance(theta, ndarray):
                 raise TypeError('param theta, though inputted, is not an array')
-            if len(theta) != len(self.layercount):
-                raise ValueError('length of theta is not what it should be accd. to layernum')
+            if len(theta) != self.partialthetalen[-1]:
+                raise ValueError('length of theta should be %d, but inputted %d' % (self.partialthetalen[-1], len(theta)))
             self.theta = theta
         else:
             # self.theta = array([(random()-0.5)/100 for i in range(self.totalthetalen)])
             self.theta = array([0 for i in range(self.totalthetalen)])
 
-    def fowardprop(self, allinp, return_a=False, return_out=False):
+    def fowardprop(self, allinp, theta=None, return_a=False, return_out=False):
+        if theta is None:
+            theta = self.theta
         if isinstance(allinp, list):
             allinp = array(allinp)
         elif not isinstance(allinp, ndarray):
@@ -99,14 +101,13 @@ class ann(object):
         if return_out:
             out = []
         for inp in allinp:
-            temp_a = [inp]
-            # debug('inputsize', inp)
+            temp_a = [append(array([1]),inp)]
             if len(inp) != self.layernum[0]:
                 raise RuntimeError('input size doesn\'t match')
             for l in range(self.layercount - 1):
                 inp = append(array([1]), inp)  # Add bias unit
                 start, end = self.partialthetalen[l], self.partialthetalen[l+1]
-                thetaseg = self.theta[start: end]
+                thetaseg = theta[start: end]
                 inp = _fowardprop(thetaseg, inp)
                 if return_a:
                     if l == self.layercount - 2:
@@ -126,28 +127,50 @@ class ann(object):
         else:
             raise RuntimeError('fowardprop call without expecting any return')
 
-    def get(self, inp):
-        return self.fowardprop([inp], return_out=True)[0]
+    def get(self, inp, a=False):
+        return self.fowardprop([inp], return_out=True, return_a=a)
 
-    def costfunc(self, _, inp, ans):
-        out = array(self.fowardprop(inp, return_out=True))
+    def costfunc(self, theta, inp, ans):
+        out = array(self.fowardprop(inp, theta, return_out=True))
         return sum(sum(ans * -log(out).T - (1 - ans) * log(1 - out).T))
 
-    def gradient_single(self, inp, ans):
+    def gradient_single(self, theta, inp, ans):
         inp = array([inp])
-        a = self.fowardprop(inp, return_a=True)[0]
+        a = self.fowardprop(inp, theta, return_a=True)[0]
         lasterror = a[-1] - ans
         delta = list(chain.from_iterable(a[-2][None].T * lasterror[None]))  # None == numpy.newaxis
         for i in range(self.layercount - 2, 0, -1):  # It is backprop!
             start, end = self.partialthetalen[i], self.partialthetalen[i+1]
-            thetaseg = self.theta[start: end].reshape(self.layernum[i]+1, self.layernum[i+1])
-            thiserror = dot(thetaseg, lasterror) * (a[i] * (1 - a[i]))
+            thetaseg = theta[start: end].reshape(self.layernum[i]+1, self.layernum[i+1])
+            d = dot(thetaseg[1:], lasterror)
+            agrad = (a[1:][i] * (1 - a[1:][i]))
+            thiserror = d * agrad
             lasterror = thiserror
             delta = list(chain.from_iterable(a[i-1][None].T * lasterror[None])) + delta
         return array(delta)
 
-    def gradient(self, _, inp, ans):
-        return [i / len(ans) for i in reduce(lambda a, b: a + b, [self.gradient_single(thisinp, thisans) for thisinp, thisans in zip(inp, ans)])]
+    def gradient(self, theta, inp, ans):
+        g = [i / len(ans) for i in reduce(lambda a, b: a + b, [self.gradient_single(theta, thisinp, thisans) for thisinp, thisans in zip(inp, ans)])]
+        return array(g)
 
     def train(self, inp, ans):
-        self.theta = minimize(self.costfunc, self.theta, args=(inp, ans), jac=self.gradient, method='BFGS').x
+        minres = minimize(self.costfunc, self.theta, args=(inp, ans), jac=self.gradient, method='BFGS')
+        self.theta = minres.x
+        print(minres)
+
+if __name__ == '__main__':
+    try:
+        a = ann([2, 2, 1])
+        data = array([[0, 0],
+                     [0, 1],
+                     [1, 0],
+                     [1, 1]])
+        ans = array([0, 1, 1, 0])
+        subdata = array([[0,0]])
+        subans = array([0])
+        a.train(subdata, subans)
+    except:
+        import traceback
+        traceback.print_exc()
+    finally:
+        __import__('code').interact(local=locals())
