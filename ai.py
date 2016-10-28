671c6cedc4d8c71c1fc2fde8656b72348255d590
dlwocks <frank_smic@hotmail.com>
2016-10-28 13:33:44 +0800

    ai.py: support random movement from bests

--- ai.py
+++ ai.py
@@ -1,24 +1,31 @@
 import logging as log
 from itertools import product
+from random import shuffle
 
 from ttthelper import isend
 
 
-def _permutation_3(row):
-    yield row[0], row[1], row[2]
-    yield row[1], row[2], row[0]
-    yield row[0], row[2], row[1]
+def donothing(x):
+    return x
 
 
-def _two_in_a_row(board, ainum):
+def _permutation(row, r):
+    pool = [(row[0], row[1], row[2]), (row[1], row[2], row[0]), (row[0], row[2], row[1])]
+    if r:
+        shuffle(pool)
+    for p in pool:
+        yield p
+
+
+def _two_in_a_row(board, ainum, r):
     '''
     Return one of positions that completes a two-in-a-row.
     (That is, a position either wins or blocks)
     Returns the one make ainum win if both player has two-in-a-row.
     '''
     temp = None
-    for row in _row_gen_pos():
-        for i, j, k in _permutation_3(row):
+    for row in _row_gen_pos(r):
+        for i, j, k in _permutation(row, r):
             if board[i[0]][i[1]] % 2 == board[j[0]][j[1]] % 2 and board[i[0]][i[1]] != 0 and board[j[0]][j[1]] != 0 and board[k[0]][k[1]] == 0:
                 if board[i[0]][i[1]] % 2 == ainum % 2:
                     return k
@@ -28,10 +35,10 @@ def _two_in_a_row(board, ainum):
     return temp
 
 
-def _fork(board, ainum, turn):
+def _fork(board, ainum, turn, r):
     tempboard = [[-100 if board[j][i] else 0 for i in range(3)]for j in range(3)]
-    for row in _row_gen_pos():
-        for i, j, k in _permutation_3(row):
+    for row in _row_gen_pos(r):
+        for i, j, k in _permutation(row, r):
             if board[i[0]][i[1]] == board[j[0]][j[1]] == 0 and board[k[0]][k[1]] != 0 and board[k[0]][k[1]] % 2 == ainum % 2:
                 tempboard[i[0]][i[1]] += 1
                 tempboard[j[0]][j[1]] += 1
@@ -39,13 +46,13 @@ def _fork(board, ainum, turn):
     return [pos for pos in product(range(3), range(3)) if tempboard[pos[0]][pos[1]] >=2]
 
 
-def _fork_opponent(board, ainum, turn):
+def _fork_opponent(board, ainum, turn, r):
     oppofork = _fork(board, (ainum % 2) + 1, turn)
     if not oppofork:
         return None
     create_tiar = set()
-    for row in _row_gen_pos():
-        for i, j, k in _permutation_3(row):
+    for row in _row_gen_pos(r):
+        for i, j, k in _permutation(row, r):
             if board[i[0]][i[1]] == board[j[0]][j[1]] == 0 != board[k[0]][k[1]] and board[k[0]][k[1]] % 2 == ainum % 2:
                 create_tiar.add((i, j))
     for posone, postwo in create_tiar:
@@ -56,15 +63,15 @@ def _fork_opponent(board, ainum, turn):
     return oppofork[0]
 
 
-def _row_gen_pos():
-    for i in range(3):
-        yield [(i, 0), (i, 1), (i, 2)]
-        yield [(0, i), (1, i), (2, i)]
-    yield [(0, 0), (1, 1), (2, 2)]
-    yield [(0, 2), (1, 1), (2, 0)]
+def _row_gen_pos(r):
+    pool = [[(i, 0), (i, 1), (i, 2)] for i in range(3)] + [[(0, i), (1, i), (2, i)] for i in range(3)] + [(0, 0), (1, 1), (2, 2), (0, 2), (1, 1), (2, 0)]
+    if r:
+        shuffle(pool)
+    for p in pool:
+        yield p
 
 
-def algorithm_wiki(board, ainum, turn):
+def algorithm_wiki(board, ainum, turn, rndfrombest=False):
     '''
     Intended to follow Newell and Simon's 1972 tic-tac-toe program's algorithm
     as shown in wikipedia.
@@ -78,15 +85,15 @@ def algorithm_wiki(board, ainum, turn):
     8. Empty Side
 
     '''
-    pos = _two_in_a_row(board, ainum)
+    pos = _two_in_a_row(board, ainum, rndfrombest)
     if pos:
         log.info('returned in _two_in_a_row')
         return pos
-    pos = _fork(board, ainum, turn)
+    pos = _fork(board, ainum, turn, rndfrombest)
     if pos:
         log.info('returned in own fork')
         return pos[0]
-    pos = _fork_opponent(board, ainum, turn)
+    pos = _fork_opponent(board, ainum, turn, rndfrombest)
     if pos:
         log.info('returned in opponent fork')
         return pos
@@ -103,11 +110,15 @@ def algorithm_wiki(board, ainum, turn):
     if board[2][0] != 0 and board[2][0] % 2 != ainum % 2 and board[0][2] == 0:
         return 0, 2
     '''
-    for i in [0, 2]:
-        for j in [0, 2]:
+    if rndfrombest:
+        f = shuffle
+    else:
+        f = donothing
+    for i in f([0, 2]):
+        for j in f([0, 2]):
             if board[i][j] == 0:
                 return i, j
-    for i, j in [(0, 1), (1, 0), (1, 2), (2, 1)]:
+    for i, j in f([(0, 1), (1, 0), (1, 2), (2, 1)]):
         if board[i][j] == 0:
             return i, j
     assert False
